65d1552d576342798ab906766c4ab4e502079cda
dgiraldoga <diegom.giraldoga@gmail.com>
2025-03-08 09:10:14 -0500

    Agregando proyecto pipeline

---
+++ my-java-api/src/test/java/com/example/myjavaapi/MyJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
